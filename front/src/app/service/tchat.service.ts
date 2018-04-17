6836789b4df6f7d235b4777cf54352c312203e5e
Thomas Betous <tbetous@gmail.com>
2018-04-18 00:24:37 +0200

    tp2

--- front/src/app/service/tchat.service.ts
+++ front/src/app/service/tchat.service.ts
@@ -5,56 +5,17 @@ import gql from 'graphql-tag'
 @Injectable()
 export class TchatService {
 
-    private messageQuery: QueryRef<any>
-
-    constructor(public apollo: Apollo) {
-        this.messageQuery = this.apollo.watchQuery({
-            query: GET_REQUEST
-        })
-        this.subscribeMessages()
-    }
+    constructor(public apollo: Apollo) {}
 
     getMessages() {
-        return this.messageQuery
-    }
-
-    saveMessage(message) {
-        return this.apollo.mutate({
-            mutation: SAVE_REQUEST,
-            variables: { message },
-            update: (store, { data: { saveMessage } }) => {
-                const data: any = store.readQuery({ query: GET_REQUEST })
-                data.getMessages.push(saveMessage)
-                store.writeQuery({ query: GET_REQUEST, data })
-            },
-            optimisticResponse: {
-                __typename: 'Mutation',
-                saveMessage: {
-                    __typename: "Message",
-                    ...message,
-                    sender: {
-                        __typename: "Sender",
-                        ...message.sender
-                    },
-                    status: "PENDING",
-                    date: Date.now()
-                }
-            }
+        return this.apollo.query({
+            query: GET_REQUEST
         })
     }
 
-    subscribeMessages() {
-        this.messageQuery.subscribeToMore({
-            document: SUBSCRIBE_MESSAGES,
-            updateQuery: (prev: any, { subscriptionData }) => {
-                let messages = prev.getMessages.slice(0)
-                messages.push(subscriptionData.data.subscribeMessages)
-                return {
-                    getMessages: messages
-                }
-            }
-        })
-    }
+    saveMessage(message) {}
+
+    subscribeMessages() {}
 }
 
 const GET_REQUEST = gql`
@@ -72,33 +33,6 @@ const GET_REQUEST = gql`
     }
 }`
 
-const SAVE_REQUEST = gql`
-mutation saveMessage($message: MessageInput!) {
-    saveMessage(message: $message) {
-        date
-        sender {
-            pseudo
-            firstName
-            lastName
-        }
-        content
-        localisation
-        status
-    }
-}`
+const SAVE_REQUEST = ``
 
-const SUBSCRIBE_MESSAGES = gql`
-subscription {
-  subscribeMessages {
-    sender {
-        pseudo
-        firstName
-        lastName
-    }
-    content
-    localisation
-    date
-    status
-  }
-}
-`
+const SUBSCRIBE_MESSAGES = ``
